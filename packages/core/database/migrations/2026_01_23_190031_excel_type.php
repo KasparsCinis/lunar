6ceb8f1a14f350d032cef9b6eb7bd04d2d21aba3
Kaspars Cinis <kcinis@apriside.com>
2026-01-23 11:04:15 +0200

    Import type

--- packages/core/database/migrations/2026_01_23_190031_excel_type.php
+++ packages/core/database/migrations/2026_01_23_190031_excel_type.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table($this->prefix.'product_variants', function (Blueprint $table) {
+        Schema::table($this->prefix.'imports', function (Blueprint $table) {
             $table->integer('type')->nullable();
         });
     }
